ef5ab4e5014fb6d9738d74654ea975df9e1bc20c
Le Dam Quang Vinh <s3818480@rmit.edu.vn>
2026-06-30 08:18:09 +0000

    Complete: Lab07 react hook 02

---
+++ session-01-html-css/session-03-react/lab-07/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Counter from './components/Counter'
+import MemoComponent from './components/MemoComponent'
+import ThemeHandler from './components/ThemeHandler'
+
+const App : React.FC = () => {
+  return (
+    <>
+      <Counter />
+      <hr />
+      <ThemeHandler />
+      <hr />
+      <MemoComponent />
+    </>
+  )
+}
+
+export default App
